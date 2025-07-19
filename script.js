ad6834b596104d7ff904fce2fafa9a0c56bee76f
Umair Amjad <umairamjad970@gmail.com>
2025-07-19 14:37:19 -0700

    Initial commit for TodoListApp

---
+++ script.js
@@ -0,0 +1,34 @@
+let addBtn = document.querySelector("#add-Btn");
+let input = document.querySelector("#taskInput");
+let ul = document.querySelector("ul");
+
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addBtn.click();
+  }
+});
+
+addBtn.addEventListener("click", () => {
+  let li = document.createElement("li");
+  li.innerText = input.value;
+
+  if (li.innerText != "") {
+    ul.appendChild(li);
+    input.value = "";
+  }
+
+  let delbtn = document.createElement("img");
+  delbtn.src = "delete-icon.svg";
+  delbtn.alt = "Delete";
+  delbtn.classList.add("delete-icon");
+  li.appendChild(delbtn);
+
+  delbtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    li.remove();
+  });
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("checked");
+  });
+});
